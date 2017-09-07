9de4dbb1fd8df155c3b672321e54f4028a47f9e7
STINGER <STINGER@DESKTOP-KVJPROG>
2017-09-07 10:11:50 +0900

    ㅁㅁ

--- Athi/src/main/java/com/kosmo/athi/MaterialController.java
+++ Athi/src/main/java/com/kosmo/athi/MaterialController.java
@@ -1,35 +1,19 @@
 package com.kosmo.athi;
 
-import javax.xml.ws.RequestWrapper;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
-/**
- * Handles requests for the application home page.
- */
 @Controller
-public class MaterialController {
+public class MaterialController {	
 	
-	private static final Logger logger = LoggerFactory.getLogger(MaterialController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	
-	
-	
-	//java컨트롤러
+	//java 컨트롤러
 	@RequestMapping("javaMaterial.do")
 	public String javamaterial(Model model) {
 		return "material/javaMaterial";
 	}
 	
-	//css컨트롤러
+	//css 컨트롤러
 	@RequestMapping("cssMaterial.do")
 	public String cssMaterial(Model model) {
 		return "material/cssMaterial";
--- Athi/src/main/java/com/kosmo/athi/command/AdminEditCommand.java
+++ Athi/src/main/java/com/kosmo/athi/command/AdminEditCommand.java
@@ -1,6 +1,5 @@
 package com.kosmo.athi.command;
 
-import java.util.ArrayList;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
